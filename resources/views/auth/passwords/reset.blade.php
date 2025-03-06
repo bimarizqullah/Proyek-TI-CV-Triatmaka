e3567d428ac693ee5d958a15e8a03834a453f973
bimarizqullah <cahyabima548@gmail.com>
2025-03-06 22:54:13 +0700

    Sprint 1 done

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,9 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="background-blur"></div>
 <div class="reset-container">
-    <h1>Reset Password</h1>
-    @if ($errors->any())
+    <!-- Bagian Kiri (Logo & Branding) -->
+    <div class="reset-left">
+        <img src="{{ asset('images/logo.png') }}" alt="Logo">
+    </div>
+
+    <!-- Bagian Kanan (Form forgot) -->
+    <div class="reset-right">
+        <h1>Lupa Password</h1>
+        @if ($errors->any())
         <div class="alert alert-danger">
             <ul>
                 @foreach ($errors->all() as $error)
@@ -12,22 +20,36 @@
             </ul>
         </div>
     @endif
-    <form action="{{ url('password/reset') }}" method="POST">
-        @csrf
-        <input type="hidden" name="token" value="{{ $token }}">
-        <div class="mb-3">
-            <label class="form-label">Email</label>
-            <input type="email" name="email" class="reset-form-control" required>
-        </div>
-        <div class="mb-3">
-            <label class="form-label">Password Baru</label>
-            <input type="password" name="password" class="reset-form-control" required>
-        </div>
-        <div class="mb-3">
-            <label class="form-label">Konfirmasi Password</label>
-            <input type="password" name="password_confirmation" class="reset-form-control" required>
-        </div>
-        <button type="submit" class="btn-submit">Reset Password</button>
-    </form>
+        <form action="{{ url('password/reset') }}" method="POST">
+            @csrf
+            <input type="hidden" name="token" value="{{ $token }}">
+            <div class="mb-3">
+                <label class="form-label">Email</label>
+                <input type="email" name="email" class="form-control" required>
+            </div>
+            <div class="mb-3">
+                <label class="form-label">Password Baru</label>
+                <input type="password" name="password" class="form-control" required>
+            </div>
+            <div class="mb-3">
+                <label class="form-label">Konfirmasi Password</label>
+                <input type="password" name="password_confirmation" class="form-control" required>
+            </div>
+            <button type="submit" class="btn-submit">Reset Password</button>
+        </form>
+    </div>
 </div>
 @endsection
+
+
+
+
+@if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
